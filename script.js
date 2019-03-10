f0daa158f08c60228aa7c688304dea72d6736253
Ronney Vargas <ronneyvargas@gmail.com>
2019-03-10 18:40:55 -0500

    Added event handler to advace to last slide

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 window.addEventListener('DOMContentLoaded', function(e) {
-    document.querySelector('hp-presentation').onclick = handlePresentationClick;
+    var presentation = document.querySelector('hp-presentation');
+    
+    presentation.onclick = handlePresentationClick;
+    presentation.addEventListener('animationend', handleAnimationEnd, false);
 });
 
 function handlePresentationClick(e) {
@@ -21,5 +24,14 @@ function handlePresentationClick(e) {
                 handlePresentationClick(e);
             }, aa);
         }
+    }
+}
+
+function handleAnimationEnd(e) {
+    var slide = e.target.closest('hp-slide');
+    var aa = slide.getAttribute('data-autoadvance');
+
+    if (aa == 'animationend' && slide.classList.contains('active')) {
+        handlePresentationClick(e);
     }
 }
